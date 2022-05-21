ee569c71379e3c824a9ef591eca69713af75bed9
therealdj97 <22632878+therealdj97@users.noreply.github.com>
2022-05-21 23:59:31 +0530

    Add files via upload

---
+++ _nuxt/static/1653157074/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftherealdj97.github.io\u002F_nuxt\u002Fstatic\u002F1653157074",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftherealdj97.github.io\u002F",assetsPath:"\u002Ftherealdj97.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
